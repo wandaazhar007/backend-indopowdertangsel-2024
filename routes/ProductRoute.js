1efeb2be9780008444476b93567028baa6103601
wanda azhar <wandaazhar@gmail.com>
2024-06-29 13:42:13 -0400

    install package react-infinite-scroll-component

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createProduct, deleteProducts, getProductById, getProducts, updateProduct, getProductCart, getAllProducts, getProductsPerCategory, searchProducts } from '../controllers/ProductController.js';
+import { createProduct, deleteProducts, getProductById, getProducts, updateProduct, getProductCart, getAllProducts, getProductsPerCategory, searchProducts, getProductInfinite } from '../controllers/ProductController.js';
 // import { verifyUser } from '../middleware/AuthUser.js';
 
 const router = express.Router();
 
 router.get('/api/products', getProducts);
+router.get('/api/products-infinite', getProductInfinite);
 router.get('/api/all-products', getAllProducts);
 router.get('/api/products-per-category', getProductsPerCategory);
 router.get('/api/products/:id', getProductById);
